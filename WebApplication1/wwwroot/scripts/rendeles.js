5a3ee374e93b7f022958592e73e32ad21d166196
Janka Balogh <jankuci2000@gmail.com>
2023-12-09 17:16:47 +0100

    folytatás2

--- WebApplication1/wwwroot/scripts/rendeles.js
+++ WebApplication1/wwwroot/scripts/rendeles.js
@@ -2,17 +2,13 @@
 const apiUrl = "https://localhost:7149/index.html";
 const apiResultsElement = document.getElementById("apiResults");
 
-fetch(apiURL)
+fetch('https://localhost:7149/index.html')
     .then(response => response.json())
-    .then(data => { displayResults(data); })
-    .catch(error => { console.error("API hivás közben hiba történt:", error); });
+    .then(data => { displayResults(data); });
+    
 
 function displayResults(results) {
-    // Töröld az esetleges előző eredményeket
-    apiResultsElement.innerHTML = "";
 
-    // Ellenőrizd, hogy az eredmények üresek-e
-    if (results.length === 0) {
-        apiResultsElement.innerHTML = "<p>Nincsenek eredmények.</p>";
-        return;
-    }
+    document.getElementById("apiResults");
+
+}
